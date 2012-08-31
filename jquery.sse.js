8e298d961cc2a057ed0c634068b630d95499d1f3
FZambia <frvzmb@gmail.com>
2012-08-31 15:43:28 +0400

    sse jquery plugin clean ups and some comments for it

--- jquery.sse.js
+++ jquery.sse.js
@@ -12,27 +12,37 @@
             return this.each(function(){
                 var self = $(this);
 			    if($.eventsource) {
+			    	// detect sse server part
 			        var sse_server = self.attr('data-sse-server') || options.sse_server;
 			        if (!sse_server) {
 			        	console.log('no sse server specified')
 			        	return false;
 			        }
+			        
+			        // detect sse channels to bind
 			        var channel_raw = self.attr('data-sse-channels');
 			        if (channel_raw.length) {
 			        	var channel_list = channel_raw.split(',')
 			        } else {
 			        	var channel_list = options.channels;
 			        }
+			        
+			        // make appropriate url
 			        var url = sse_server;
 			        if (channel_list.length) {
 			        	var channels =  $.param({'channels': channel_list});
 			        	url = url + '?' + channels;
 			        }
+
 			        if (channel_list.length === 0 && options.debug === true) {
 			        	console.log('no channels specified, you will listen to default channel');
 			        }
-			        console.log(url);
 
+			        if (options.debug === true) {
+			        	console.log(url);
+			        }
+
+					// create EventSource object using Rick Waldron's jquery.eventsource.js
 			        $.eventsource({
 			            label: 'sys-sse',
 			            url: url,
@@ -47,6 +57,7 @@
 			            	if (options.debug === true) {
 			            		console.log(msg);	
 			            	}
+			            	// msg can be null in case of ping sse messages
 			            	if (msg) {
 			                	var event = 'sse.';
 								var type = msg[0];
@@ -63,10 +74,10 @@
 			            	}
 			                self.trigger('sse.error', msg);
 			            }
-			        });	
+			        });
 			    }
             });
         }
     })
 })
-(jQuery) 
+(jQuery)
