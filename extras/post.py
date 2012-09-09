518035a98a4f73e7e36889ccb6d745d00877c3d1
FZambia <frvzmb@gmail.com>
2012-09-09 10:39:50 +0400

    test scrips moved to extras folder

---
+++ extras/post.py
@@ -0,0 +1,20 @@
+# coding: utf:8
+"""
+help script to test http broker
+"""
+import urllib
+import urllib2
+import json
+import sys
+
+channel = sys.argv[1]
+
+try:
+    port = sys.argv[2]
+except:
+    port = '8888'
+
+if __name__ == '__main__':
+    data = {'channel': channel, 'message':json.dumps([123, 124])}
+    encoded = urllib.urlencode(data)
+    urllib2.urlopen("http://localhost:%s/publish" % port, encoded, timeout=2)
--- extras/pub.py
+++ extras/pub.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
+"""
+help script to test amqp broker.
+needs pika python module
+"""
 import pika
 import sys
 import json
 
 queue_name = sys.argv[1]
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-          host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='sse-cyclone-server',
@@ -16,5 +19,6 @@ message = json.dumps(['comment', '123'])
 channel.basic_publish(exchange='sse-cyclone-server',
                       routing_key=queue_name,
                       body=message)
+
 print " [x] Sent %r" % (message,)
 connection.close()
