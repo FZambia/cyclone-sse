8bc6e838c41be321cbed532a1d3aa610b9f3dab7
FZambia <frvzmb@gmail.com>
2012-09-22 12:20:02 +0400

    0.6.5 version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ else:
 
 setup(
     name="cyclone-sse",
-    version="0.6.1",
+    version="0.6.5",
     author="Alexandr Emelin",
     author_email="frvzmb@gmail.com",
     url="https://github.com/FZambia/cyclone-sse",
