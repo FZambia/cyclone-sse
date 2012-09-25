6a49d55e6a8071ba57802dea02e640289b7efe8f
FZambia <frvzmb@gmail.com>
2012-09-25 16:20:36 +0400

    more powerful sse jquery plugin

--- media/jquery.sse.js
+++ media/jquery.sse.js
@@ -1,79 +1,119 @@
-(function($){
-    $.fn.extend({
+;(function(jQuery){
+    jQuery.extend({
         sse: function(options) {
             var defaults = {
-                address: null,
-                channels: [],
+            	url: '/sse/',
+            	selector: '.sse',
+            	attr: 'data-sse-channels',
+                debug: false,
+                label: "default",
                 type: "json",
-                label: 'sse-channel',
-                debug: false
+                eventPrefix: 'sse.'
             }
 
-            var options = $.extend(defaults, options);
+            var options = jQuery.extend(defaults, options);
 
-            return this.each(function(){
-                var self = $(this);
-			    if($.eventsource) {
-			    	// detect sse server address part
-			        var url = self.attr('data-sse-address') || options.address;
-			        if (!url) {
-			        	console.log('no sse server address specified')
-			        	return false;
-			        }
+			function init() {
+				if (!jQuery.eventsource) {
+					console.log("jquery eventsource polyfill not found");
+					return;
+				}
 
-			        // detect sse channels to bind
-			        var channel_raw = self.attr('data-sse-channels');
-			        if (channel_raw.length) {
-			        	var channel_list = channel_raw.split(',')
-			        } else {
-			        	var channel_list = options.channels;
-			        }
+				// now we should find all channels we want to listen
+				var channels = [];
+				var handlers = jQuery(options.selector);
+				var compliance = {};
+				if (handlers.length === 0) {
+					if (options.debug === true) {
+						console.log("no eventsource handlers found on page");
+					}
+					return;
+				}
+				handlers.each(function(index, element) {
+					var handler = jQuery(element);
+					handler_channels = handler.attr(options.attr);
+					if (handler_channels.length) {
+						var channel_list = handler_channels.split(',')
+						for (i in channel_list) {
+							var c = jQuery.trim(channel_list[i]);
 
-			        if (channel_list.length) {
-			        	var channels =  $.param({'channels': channel_list}, true);
-			        } else {
-			        	var channels = null;
-			        }
-
-			        if (channel_list.length === 0 && options.debug === true) {
-			        	console.log('no channels specified, you will listen to default channel');
-			        }
-
-			        if (options.debug === true) {
-			        	console.log(url);
-			        }
-
-					// create EventSource object using Rick Waldron's jquery.eventsource.js
-			        $.eventsource({
-			            label: options.label,
-			            url: url,
-			            data: channels,
-			            dataType: options.type,
-						open: function() {
-							if (options.debug === true) {
-								console.log('sse connection opened');
+							if (!(c in compliance)) {
+								compliance[c] = [];
 							}
-						    self.trigger('sse.open');
-						},
-						message: function(msg) {
-							if (options.debug === true) {
-								console.log(msg);
+							compliance[c].push(handler);
+
+							if (jQuery.inArray(c, channels) === -1) {
+								channels.push(c);
 							}
-							// msg can be null in case of ping sse messages
-							if (msg) {
-						    	self.trigger('sse.message', msg);
-						    }
-						},
-						error: function(err) {
-							if (options.debug === true) {
-								console.log('sse connection error:');
-								console.log(err);
-							}
-						    self.trigger('sse.error', msg);
-			            }
-			        });
-			    }
-            });
+						}
+					}
+				});
+				if (channels.length === 0) {
+					if (options.debug === true) {
+						console.log("no channels found");
+					}
+					return;
+				}
+
+		        // make appropriate query
+		        var query = jQuery.param({'channels': channels}, true);
+		        if (options.debug === true) {
+		        	console.log('sse url: ' + options.url + '?' + query);
+		        }
+
+				// create EventSource object using Rick Waldron's jquery.eventsource.js
+		        jQuery.eventsource({
+		            label: options.label,
+		            url: options.url,
+		            data: query,
+		            dataType: options.type,
+
+		            open: function() {
+		            	if (options.debug === true) {
+		            		console.log('sse connection opened');
+		            	}
+		                handlers.each(function(index, element){
+		                	 jQuery(element).trigger('sse.open');
+		                });
+		            },
+
+		            error: function(err) {
+		            	if (options.debug === true) {
+		            		console.log('sse connection error');
+		            		console.log(msg);
+		            	}
+		            	handlers.each(function(index, element){
+		                	jQuery(element).trigger('sse.error', err);
+		                });
+		            },
+
+		            message: function(msg, event) {
+		            	if (options.debug === true) {
+		            		console.log(msg);
+		            		console.log(event);
+		            	}
+		            	// msg can be null in case of ping sse messages
+		            	if (msg) {
+		                 	var customEvent = options.eventPrefix;
+							var eventType = msg[0];
+							var eventData = msg[1];
+		                	customEvent += eventType;
+		                	if (options.debug === true) {
+		                		console.log("triggering event: " + customEvent);
+		                	}
+		                	if (eventType in compliance) {
+		                		var eventHandlers = compliance[eventType];
+		                		jQuery.each(eventHandlers, function(index, element){
+		                			jQuery(element).trigger(customEvent, eventData);
+		                		});
+		                	}
+		                }
+		            }
+		        });
+			}
+
+			return init();
+
         }
     })
 })
---
+++ media/jquery.sse.simple.js
@@ -0,0 +1,80 @@
+(function($){
+    $.fn.extend({
+        sse: function(options) {
+            var defaults = {
+                address: null,
+                channels: [],
+                type: "json",
+                label: 'sse-channel',
+                debug: false
+            }
+
+            var options = $.extend(defaults, options);
+
+            return this.each(function(){
+                var self = $(this);
+			    if($.eventsource) {
+			    	// detect sse server address part
+			        var url = self.attr('data-sse-address') || options.address;
+			        if (!url) {
+			        	console.log('no sse server address specified')
+			        	return false;
+			        }
+
+			        // detect sse channels to bind
+			        var channel_raw = self.attr('data-sse-channels');
+			        if (channel_raw.length) {
+			        	var channel_list = channel_raw.split(',')
+			        } else {
+			        	var channel_list = options.channels;
+			        }
+
+			        if (channel_list.length) {
+			        	var channels =  $.param({'channels': channel_list}, true);
+			        } else {
+			        	var channels = null;
+			        }
+
+			        if (channel_list.length === 0 && options.debug === true) {
+			        	console.log('no channels specified, you will listen to default channel');
+			        }
+
+			        if (options.debug === true) {
+			        	console.log(url);
+			        }
+
+					// create EventSource object using Rick Waldron's jquery.eventsource.js
+			        $.eventsource({
+			            label: options.label,
+			            url: url,
+			            data: channels,
+			            dataType: options.type,
+						open: function() {
+							if (options.debug === true) {
+								console.log('sse connection opened');
+							}
+						    self.trigger('sse.open');
+						},
+						message: function(msg) {
+							if (options.debug === true) {
+								console.log(msg);
+							}
+							// msg can be null in case of ping sse messages
+							if (msg) {
+						    	self.trigger('sse.message', msg);
+						    }
+						},
+						error: function(err) {
+							if (options.debug === true) {
+								console.log('sse connection error:');
+								console.log(err);
+							}
+						    self.trigger('sse.error', msg);
+			            }
+			        });
+			    }
+            });
+        }
+    })
+})
+(jQuery)
