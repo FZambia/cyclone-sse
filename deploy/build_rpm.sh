f82daf99da7a05520a01e1ec0067de210f7fcd75
FZambia <frvzmb@gmail.com>
2013-03-27 15:46:19 +0400

    deploy and bump version

--- deploy/build_rpm.sh
+++ deploy/build_rpm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rpmbuild -bb cyclone_sse.spec --define "version 0.7.4" --define "release `date +%s`"
+rpmbuild -bb cyclone_sse.spec --define "version 0.7.5" --define "release `date +%s`"
